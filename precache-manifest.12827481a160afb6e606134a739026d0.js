3c93bf92ef2442f161d1eea363e0556730871b80
Zhaoyu Qiao <qiao.zy7@gmail.com>
2020-02-08 09:35:19 -0500

    Updates

---
+++ precache-manifest.12827481a160afb6e606134a739026d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52e627089bb3874b84d03152d2e02200",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "36e45d86af4a3591ae12",
+    "url": "/clicky-game/static/css/main.23b04409.chunk.css"
+  },
+  {
+    "revision": "05c48e8c4e644dd36c85",
+    "url": "/clicky-game/static/js/2.67167996.chunk.js"
+  },
+  {
+    "revision": "94b03e19a4c9eb66db847f3cca1e03ba",
+    "url": "/clicky-game/static/js/2.67167996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36e45d86af4a3591ae12",
+    "url": "/clicky-game/static/js/main.3e0b71b9.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
